706648203e054cf1e0ea3ebd3cf586e41f101d38
gllrmoliva <gllrmoliva42@gmail.com>
2025-11-13 00:33:59 -0300

    feat: nvim(illuminate and lsp beta)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,8 +8,8 @@ vim.opt.autoindent = true           -- nueva línea hereda indentación
 vim.opt.number = true               -- mostrar números de línea
 vim.opt.colorcolumn = "80"         -- columna de referencia en 100
 vim.cmd("filetype plugin indent on") -- activación de indentado por tipo de archivo
-vim.cmd("syntax on")                -- resaltar sintaxis
-vim.opt.mouse = "a"                 -- habilitar ratón
+vim.cmd("syntax on")                -- resaltar sintaxis 
+vim.opt.mouse = "a"                 -- habilitar ratón 
 vim.opt.clipboard = "unnamedplus"   -- usar portapapeles del sistema
 vim.cmd("filetype plugin on")       -- plugins por tipo de archivo
 vim.opt.cursorline = true           -- resaltar línea del cursor
@@ -39,21 +39,24 @@ end
 bootstrap_pckr()
 
 require('pckr').add{
-	"nvim-treesitter/nvim-treesitter",      --sintax color
-	"nvim-tree/nvim-tree.lua",              --archivos
-    "nvim-tree/nvim-web-devicons",          --iconos
-    "nvim-lua/plenary.nvim",                --dependencia telescope
-    "nvim-telescope/telescope.nvim",        --selección de buffer
-    "nvim-lualine/lualine.nvim",            --linea de abajo más bonita
-    "catppuccin/nvim",                      --colorscheme
-    "lewis6991/gitsigns.nvim",              --merge en git
-    "goolord/alpha-nvim",                   --greeter (homepage)
-    "echasnovski/mini.icons",               --dependencia alpha-nvim
-    "meatballs/notebook.nvim",              --visualizador de ipynb
-    "akinsho/bufferline.nvim",              --buffers arriba
-    "MeanderingProgrammer/render-markdown.nvim", -- markdown
-    "lukas-reineke/indent-blankline.nvim",       -- lineas en scopes
-    "RRethy/vim-illuminate"                     -- iluminar variables con mismo nombre
+	"nvim-treesitter/nvim-treesitter",              --sintax color
+	"nvim-tree/nvim-tree.lua",                      --archivos
+    "nvim-tree/nvim-web-devicons",                  --iconos
+    "nvim-lua/plenary.nvim",                        --dependencia telescope
+    "nvim-telescope/telescope.nvim",                --selección de buffer
+    "nvim-lualine/lualine.nvim",                    --linea de abajo más bonita
+    "catppuccin/nvim",                              --colorscheme
+    "lewis6991/gitsigns.nvim",                      --merge en git
+    "goolord/alpha-nvim",                           --greeter (homepage)
+    "echasnovski/mini.icons",                       --dependencia alpha-nvim
+    "meatballs/notebook.nvim",                      --visualizador de ipynb
+    "akinsho/bufferline.nvim",                      --buffers arriba
+    "MeanderingProgrammer/render-markdown.nvim",    -- markdown
+    "lukas-reineke/indent-blankline.nvim",          -- lineas en scopes
+    "RRethy/vim-illuminate",                        -- iluminar variables con mismo nombre
+    "mason-org/mason.nvim",                         -- LSP
+    "neovim/nvim-lspconfig",                        -- LSP
+    "mason-org/mason-lspconfig.nvim"                -- LSP
 }
 
 require("plugins.telescope")
@@ -62,11 +65,16 @@ require("plugins.catppuccin")
 require("plugins.gitsigns")
 require("plugins.nvimtree")
 require("plugins.treesitter")
+require("plugins.illuminate")
 
 require'alpha'.setup(require'alpha.themes.startify'.config)
 require('notebook').setup()
 require("bufferline").setup{}
 require("ibl").setup()
 
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+})
+require("plugins.lspconfig")
 -- Para actualizar :Pckr sync
-
